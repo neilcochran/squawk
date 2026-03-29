482470b779e64c4b584aa7d58982863d9f7e7e1c
neilcochran <dev.neil.cochran@pm.me>
2026-03-29 00:37:35 -0400

    add ArtccSector interface

--- packages/types/src/airspace.ts
+++ packages/types/src/airspace.ts
@@ -1,1 +1,11 @@
-// TODO: implement ArtccSector, PolygonRing, and UnknownAirspace types
+import type { Polygon } from 'geojson';
+
+export interface ArtccSector {
+  centerName: string;
+  facilityId: string;
+  sectorId: string;
+  floorFt: number;
+  isSurfaceFloor: boolean;
+  ceilingFt: number;
+  boundary: Polygon;
+}
