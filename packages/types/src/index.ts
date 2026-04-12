6c026f083d97fb04c492877e05d3b4f1e7c84960
neilcochran <dev.neil.cochran@pm.me>
2026-04-12 12:52:34 -0400

    pre initial npm publish clean up

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,7 +3,6 @@
  * Re-export core shared data model types used by Squawk packages.
  */
 export * from './aircraft.js';
-export * from './adsb-source.js';
 export * from './airspace.js';
 export * from './airport.js';
 export * from './airway.js';
